ada35fbef91251a3589dfd85b8af54a5d7929572
dong-geon-Lee <ch9716@naver.com>
2023-05-06 14:31:49 +0900

    Demo 컴포넌트 구현

---
+++ src/components/Demo.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center">
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Enter
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
